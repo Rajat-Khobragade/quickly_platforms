d15bf43530d7d058ae7fbe3674cff61f91506324
Rajat Khobragade <rajat.khobragade@optimite.net>
2024-06-10 16:13:40 +0530

    port change 3000 to 4000

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use("/",route)
 mongoose.connect('mongodb+srv://avenger:rajatrajat12@cluster0.wuyw0.mongodb.net/quickly_platforms');
 
-const port = 3000
+const port = 4000
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
